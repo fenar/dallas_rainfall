e80f6824ec3b9803bb04b9b702f42f7b3d7ccff3
Fatih Nar <fenar@yahoo.com>
2019-03-30 17:17:47 -0500

    Update append_data.py

--- function/append_data.py
+++ function/append_data.py
@@ -1,33 +1,19 @@
-#this script appends live Chicago traffic data into BigQuery, there will be duplicates
-#but that's accounted for with a saved view removing duplicates using SQL
+#this module appends live Dallas Rainfall  data into BigQuery, there will be duplicates
+#we will handle the duplicates over Datalab and save in to another BQ Table for presentation over Data Studio
+#FENAR
 
-from __future__ import print_function, absolute_import #package to smooth over python 2 and 3 differences
-import pandas as pd #package for dataframes
-from sodapy import Socrata #package for open source api
-from google.datalab import Context #package for datalab
+from __future__ import print_function, absolute_import 
+import pandas as pd 
+from sodapy import Socrata
+from google.datalab import Context 
 import time
 from datetime import datetime, timedelta
-import logging #package for error logging
+import logging 
 
-#tracks error messaging
 logging.basicConfig(level=logging.DEBUG)
 
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
-#client = Socrata("data.cityofchicago.org", None)
-
-#indent the run function by 1 tab
 def run():
-# Example authenticated client (needed for non-public datasets):
 	client = Socrata("www.dallasopendata.com", None)
-	                 
-	# First 2000 results, returned as JSON from API / converted to Python list of
-	# dictionaries by sodapy.
 	results = client.get("xm9r-xkdu", limit=2000)
-	
-	# Convert to pandas DataFrame
 	results_df = pd.DataFrame.from_records(results)
-	
-	
-	#have this go directly into bigquery syntax-https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_gbq.html	
         results_df.to_gbq('dallas.rainfall', 'fenar-bigdatadallas-demo', chunksize=None, verbose=True, if_exists='append')
